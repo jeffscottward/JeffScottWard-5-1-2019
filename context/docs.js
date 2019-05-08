08180396c4d626240926ed655178cc7e1398fabd
Jeff Ward <jsward.17@gmail.com>
2019-05-08 14:48:21 -0400

    move stuff into context file

--- context/docs.js
+++ context/docs.js
@@ -1,5 +1,43 @@
 import React from "react";
 export const DocsContext = React.createContext({});
 
+// Create Docs Actions
+export const DocsActions = {
+  SET_DOC: "SET_DOC",
+  REMOVE_DOC: "REMOVE_DOC",
+  SET_VISIBLE_DOCS: "SET_VISIBLE_DOCS",
+}
 
+export function DocsReducer(docs, action) {
+  switch (action.type) {
+    case "SET_DOC":
+      docs.uploads.push(action.payload)
+      docs.onlyVisible.push(action.payload)
+      return
+    case "REMOVE_DOC":
+      // Fancy algo to remove parent obj by attribute 
+      // https://stackoverflow.com/a/26327271
+
+      // Update docs.uploads
+      var removeIndex = docs.uploads.map(item => item.id).indexOf(action.payload)
+      if (removeIndex >= 0) { docs.uploads.splice(removeIndex, 1) }
+
+      // Update docs.onlyVisible
+      if (!!docs.onlyVisible && docs.onlyVisible.length > 0) {
+        var removeIndex = docs.onlyVisible.map(item => item.id).indexOf(action.payload)
+        if (removeIndex >= 0) { docs.onlyVisible.splice(removeIndex, 1) }
+      }
+      return
+    case "SET_VISIBLE_DOCS":
+      // If no search term
+      if (action.payload.length === 0) {
+        docs.onlyVisible = docs.uploads
+      } else {
+        docs.onlyVisible = action.payload
+      }
+      return
+    default:
+      return docs
+  }
+}
 
